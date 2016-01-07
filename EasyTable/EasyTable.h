031a19885f63b0e1a4820758e9c1f3a01a8c53cc
Serge Neskoromny <serge.nes@gmail.com>
2016-01-07 21:21:56 +0200

    first commit

---
+++ EasyTable/EasyTable.h
@@ -0,0 +1,19 @@
+//
+//  EasyTable.h
+//  EasyTable
+//
+//  Created by Serge Neskoromny on 1/7/16.
+//  Copyright © 2016 Sergey Nes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyTable.
+FOUNDATION_EXPORT double EasyTableVersionNumber;
+
+//! Project version string for EasyTable.
+FOUNDATION_EXPORT const unsigned char EasyTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyTable/PublicHeader.h>
+
+
